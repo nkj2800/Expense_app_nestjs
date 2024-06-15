cd3a84e636a856dc56a1a43d8936a34bcd378f38
Noel <nkj2800@gmail.com>
2024-06-15 10:58:12 +0530

    feat: add report controllers with dummy values

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common"
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports() {
+    return []
+  }
+
+  @Get(':id')
+  getAReport() {
+    return {}
+  }
+
+  @Post()
+  createReport() {
+    return "Created"
+  }
+
+  @Put(':id')
+  updateReport() {
+    return "Updated"
+  }
+
+  @Delete(':id') 
+  removeReport() {
+    return "Deleted"
   }
 }
